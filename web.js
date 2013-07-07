1b6d9360b0cc8397bab2f9dec54ed78bf6714f18
charlewn <foonlife@gmail.com>
2013-07-07 03:02:49 +0000

    add new content

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var content = fs.readFileSync("index.html");
+
+var buffer = new Buffer(content, "utf-8");
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
