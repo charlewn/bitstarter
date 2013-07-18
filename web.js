1fb3c6713a660a6cccfad46c898e4fe3d1a3a2ea
Charles Ng <foonlife@gmail.com>
2013-07-18 17:40:49 +0000

    add new files for hw4 part2

--- web.js
+++ web.js
@@ -18,6 +18,9 @@ var port = process.env.PORT || 8080;
 //using the css directory
 app.use("/css",express.static(__dirname+"/css"));
 
+//using the img directory
+app.use("/img",express.static(__dirname+"/img"));
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
